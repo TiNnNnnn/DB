d477f6f67cb688fe09c5df6dfb44ac908b404f80
Ornamrr <18827906801@139.com>
2024-06-01 14:30:59 +0800

    debug

--- src/storage/buffer_pool_manager.cpp
+++ src/storage/buffer_pool_manager.cpp
@@ -96,12 +96,11 @@ Page* BufferPoolManager::fetch_page(PageId page_id) {
         }
         Page *page = &pages_[frame_id];
         // 2. 若获得的可用 frame 存储的为 dirty page，则调用 updata_page 将 page 写回到磁盘
-        if (page->is_dirty_) {
+        if (page->is_dirty_) 
             update_page(page, page_id, frame_id);
-        }else{
-            // 3. 调用 disk_manager_ 的 read_page 读取目标页到 frame
-            disk_manager_->read_page(page_id.fd, page_id.page_no, page->data_, PAGE_SIZE);
-        }
+        // 3. 调用 disk_manager_ 的 read_page 读取目标页到 frame
+        disk_manager_->read_page(page_id.fd, page_id.page_no, page->data_, PAGE_SIZE);
+        
         // 4. 固定目标页，更新 pin_count_
         replacer_->pin(frame_id);
         page->id_ = page_id;
